1733ecabc9ec2cb7c17a98821181f791d5524f84
Bobbys456 <bobbysullivan61@gmail.com>
2023-02-18 11:37:15 -0500

    now server deletes files when restarting

--- data2.py
+++ data2.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd 
 from nltk.corpus import stopwords
-import os
+import os, shutil
 import string
 #from pyvirtualdisplay import Display
 nltk_stopwords = set(stopwords.words('english'))
@@ -160,6 +160,16 @@ def mergeDataFiles(goodOrBad):
     
 def main(): 
         
+        folder = datafolder
+        for filename in os.listdir(folder):
+                file_path = os.path.join(folder, filename)
+        try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+        except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
 
         file = open("companies.txt", "r")
         data = file.read()
